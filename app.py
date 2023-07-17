ac36306a7057934aaff700e202b0a90fe5f38d59
Giacomo Pugliese <puglieseg24@gmail.com>
2023-07-16 23:42:11 -0400

    new3

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pyheif
 from PIL import Image
+import streamlit as st
+import os
 
 def heic_to_jpg(heic_path, jpg_path):
     heif_file = pyheif.read(heic_path)
@@ -12,9 +14,19 @@ def heic_to_jpg(heic_path, jpg_path):
         heif_file.stride,
     )
     image.save(jpg_path, format="JPEG")
+    return jpg_path
 
 # Replace with your file paths
 heic_file_path = 'IMG_0695.HEIC'
 jpg_file_path = 'file.jpg'
 
-heic_to_jpg(heic_file_path, jpg_file_path)
+jpg_file_path = heic_to_jpg(heic_file_path, jpg_file_path)
+
+# Create a button for the download
+with open(jpg_file_path, 'rb') as f:
+    btn = st.download_button(
+        label="Download JPG",
+        data=f,
+        file_name=os.path.basename(jpg_file_path),
+        mime='image/jpeg',
+    )
